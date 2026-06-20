ba51e4c6d083171f65cec6715a2d0885a86ad532
totemoflol <totemoflol4@gmail.com>
2026-06-20 14:05:07 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,8 @@
- local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/jodta/my-scripts/refs/heads/main/Other/Library2"))()
+ -- loader.lua
+local Library = (loadstring(game:HttpGet("https://raw.githubusercontent.com/jodta/my-scripts/refs/heads/main/Other/Library2"))()) or getgenv().Library
 
-local window = Library:window({Logo="rbxassetid://"})
-local mainpage = Window:page({ Icon = "rbxassetid://" })
-local player = Window:page({ Icon = "rbxassetid://" })
+-- 2) Set the Window Name and Logo
+getgenv().Window = Library:Window({
+    Name = "ToT Spectrum", -- If the library supports a text title
+    Logo = "rbxassetid://77749228793011" -- Custom logo image ID
+})
