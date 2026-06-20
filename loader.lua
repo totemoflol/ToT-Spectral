c8202f92fc01bddcfe31e3c178c34c240cbc34e1
totemoflol <totemoflol4@gmail.com>
2026-06-20 14:06:19 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,6 +3,6 @@ local Library = (loadstring(game:HttpGet("https://raw.githubusercontent.com/jodt
 
 -- 2) Set the Window Name and Logo
 getgenv().Window = Library:Window({
-    Name = "ToT Spectrum", -- If the library supports a text title
+    Name = "TØT Spectrum", -- If the library supports a text title
     Logo = "rbxassetid://77749228793011" -- Custom logo image ID
 })
